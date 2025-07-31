9cc999a186eec900e7e3a43990babe63d85d5de1
Artem <agrigoren@sfedu.ru>
2025-07-31 20:27:23 +0300

    Create ChatService without auth

--- backend/chatService/src/main/java/com/artimelo/Main.java
+++
@@ -1,8 +0,0 @@
-package com.artimelo;
-
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ backend/chatService/src/main/java/com/artimelo/repository/ChatMessageRepository.java
@@ -0,0 +1,16 @@
+package com.artimelo.repository;
+
+import com.artimelo.model.ChatMessage;
+import com.artimelo.model.MessageStatus;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository
+        extends MongoRepository<ChatMessage, String> {
+
+    long countBySenderIdAndRecipientIdAndStatus(
+            String senderId, String recipientId, MessageStatus status);
+
+    List<ChatMessage> findByChatId(String chatId);
+}
