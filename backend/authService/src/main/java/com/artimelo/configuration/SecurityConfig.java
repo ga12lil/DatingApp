3adf4307346b4959a7f4ef36ba0b76d52919cdef
Artem <agrigoren@sfedu.ru>
2025-08-08 19:29:11 +0300

    fix cors config

--- backend/authService/src/main/java/com/artimelo/configuration/SecurityConfig.java
+++ backend/authService/src/main/java/com/artimelo/configuration/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.artimelo.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -20,7 +22,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()  // login, register открыты
-                        .anyRequest().authenticated()             // всё остальное — с токеном
+                        //.anyRequest().authenticated()             // всё остальное — с токеном
                 );
 
         return http.build();
--- backend/chatService/src/main/java/com/artimelo/configuration/SecurityConfig.java
+++ backend/chatService/src/main/java/com/artimelo/configuration/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +27,24 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // отключаем CSRF для stateless API
+                .cors(httpSecurityCorsConfigurer ->
+                        httpSecurityCorsConfigurer.configurationSource(request ->
+                                {
+                                    final CorsConfiguration config = new CorsConfiguration();
+                                    config.setAllowCredentials(true);
+                                    config.setAllowedOriginPatterns(List.of("*"));
+                                    config.addAllowedHeader("*");
+                                    config.addAllowedMethod("OPTIONS");
+                                    config.addAllowedMethod("HEAD");
+                                    config.addAllowedMethod("GET");
+                                    config.addAllowedMethod("PUT");
+                                    config.addAllowedMethod("POST");
+                                    config.addAllowedMethod("DELETE");
+                                    config.addAllowedMethod("PATCH");
+                                    return config;
+                                }
+                        )
+                )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
